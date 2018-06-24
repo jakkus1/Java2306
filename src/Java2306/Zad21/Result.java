dbb3b8473c58d38230fe2b5b9be1bb89e050dbf2
Jakub Kus <jakkus@op.pl>
2018-06-24 09:25:55 +0200

    getingnore add

--- src/Java2306/Zad21/Result.java
+++ src/Java2306/Zad21/Result.java
@@ -6,5 +6,6 @@ public class Result {
 
         Zad21Rekurencja num = new Zad21Rekurencja();
         System.out.println("factorial = " + num.factorial(10));
+        System.out.println("tutaj");
     }
 }
