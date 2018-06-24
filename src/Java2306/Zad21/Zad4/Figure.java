19139a4e57c68f4697e2b9d0314ef0e1df31faae
Jakub Kus <jakkus@op.pl>
2018-06-24 09:58:43 +0200

    Figure changes

--- src/Java2306/Zad21/Zad4/Figure.java
+++ src/Java2306/Zad21/Zad4/Figure.java
@@ -31,12 +31,12 @@ public class Figure {
     }
 
     static void figure3 (int n){
-        for(int i = 0; i<= n; i++){
-            for(int j = 0; j <= n; j++){
-                if((i == 0 && j == 0) || (i == n && j == n)){
-                    System.out.print("******");
+        for(int i = 0; i< n; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 || j == 0 || i == n - 1 || j == n - 1){
+                    System.out.print("*");
                 } else {
-                    System.out.print("*    *");
+                    System.out.print(" ");
                 }
             }
             System.out.println();
