aa736378385b73d6cdf9f3f51aa24b9712a91cb0
Jakub Kus <jakkus@op.pl>
2018-07-08 11:37:09 +0200

    New commit 08.07

--- src/Java0707/Heap/Heap.java
+++ src/Java0707/Heap/Heap.java
@@ -14,28 +14,30 @@ public class Heap {
     }
 
 
-    public void add (int data){
-        tree[lastIndex] = data;
+    public void add (int index) {
+        tree[lastIndex] = index;
         int i = lastIndex++;
         int tmp;
         int parent;
-        while (i > 0){
+        while (i > 0) {
             parent = getParent(i);
-            if(tree[parent] < tree[i]) {
+            if (tree[parent] < tree[i]) {
                 tmp = tree[parent];
                 tree[parent] = tree[i];
                 tree[i] = tmp;
-            }else{
+            } else {
                 break;
             }
             i = parent;
         }
 
     }
-    int getLeftChild(int index){
+
+    int getLeftChild (int index) {
         return tree[getLeftChildIndex(index)];
     }
-    int getRightChild(int index){
+
+    int getRightChild (int index) {
         return tree[getRightChildIndex(index)];
     }
 
@@ -62,11 +64,11 @@ public class Heap {
         }
     }
 
-    private boolean isRightChild(int index) {
+    private boolean isRightChild (int index) {
         return (2 * index + 2) < lastIndex;
     }
 
-    int getParent(int index) {
+    int getParent (int index) {
         if (index != 0) {
             return (index - 1) / 2;
         } else {
@@ -74,13 +76,115 @@ public class Heap {
         }
     }
 
-    boolean isLeaf(int index) {
+    boolean isLeaf (int index) {
         return !isLeftChild(index);
     }
 
     @Override
-    public String toString() {
-        return Arrays.toString(tree);
+    public String toString () {
+        return Arrays.toString(Arrays.copyOfRange(tree, 0, lastIndex));
     }
+
+   public int remove (int index) {
+        int parent;
+        int r;
+
+        r = tree[index];
+
+        tree[index] = tree[lastIndex-1];
+
+        lastIndex--;
+
+        parent = (index - 1) / 2;
+
+
+        if (index  == 0 || tree[parent] < tree[index])
+            HeapMoveDown(index);
+        else
+            HeapMoveUp(index);
+
+        return r;
+
+    }
+
+    void HeapMoveUp (int index) {
+        int parent;
+        int help;
+
+        while (index != 0) {
+
+            parent = (index - 1) / 2;
+
+            if (tree[index] < tree[parent]) {
+                help = tree[parent];
+                tree[parent] = tree[index];
+                tree[index] = help;
+
+
+                index = parent;
+            } else {
+                break;
+            }
+
+        }
+    }
+
+    void HeapMoveDown (int index) {
+
+        {
+            int child1;
+            int child2;
+            int help;
+
+
+            while (2 * index + 1 <= lastIndex ) {
+                child1 = 2 * index + 1;                 // lewe dziecko
+                child2 = 2 * index + 2;               // prawe dziecko
+
+                if (child2 <= lastIndex) {
+
+                    if (tree[index] < tree[child1] && tree[index] < tree[child2]) {
+
+                        break;
+                    } else {
+
+                        if (tree[child1] < tree[child2]) {
+
+                            help = tree[index];
+                            tree[index] = tree[child1];
+                            tree[child1] = help;
+
+                            index = child1;
+                        } else {
+
+                            help = tree[index];
+                            tree[index] = tree[child2];
+                            tree[child2] = help;
+
+                            index = child2;
+                        }
+                    }
+                } else {
+
+                    if (tree[index] < tree[child1]) {
+
+                        break;
+                    } else {
+
+                        help = tree[index];
+                        tree[index] = tree[child1];
+                        tree[child1] = help;
+
+                        index = child1;
+                    }
+                }
+            }
+        }
+
+    }
+
+
+
+
 }
 
--- src/Java0707/Heap/HeapTest.java
+++ src/Java0707/Heap/HeapTest.java
@@ -2,12 +2,14 @@ package Java0707.Heap;
 
 public class HeapTest {
     public static void main (String[] args) {
-       Heap heap = new Heap(5);
-       heap.add(3);
-       heap.add(6);
-       heap.add(8);
-       heap.add(12);
-       heap.add(2);
+        Heap heap = new Heap(5);
+        heap.add(22);
+        heap.add(6);
+        heap.add(8);
+        heap.add(12);
+        heap.add(2);
+        System.out.println(heap);
+        heap.remove(0);
         System.out.println(heap);
 
     }
